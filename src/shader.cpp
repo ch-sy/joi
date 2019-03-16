8f36cfd2fbd041fe1a701c15a5b20c4a545305e2
Christian Seybert <christian.seybert@stud.tu-darmstadt.de>
2019-03-16 13:38:41 +0100

    Fix shader error log

--- src/shader.cpp
+++ src/shader.cpp
@@ -2,129 +2,6 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <vector>
 
-Shader::Shader(std::string vertexPath, std::string fragmentPath) {
-	// 1. retrieve the vertex/fragment source code from filePath
-	std::string vertexCode;
-	std::string fragmentCode;
-	std::ifstream vShaderFile;
-	std::ifstream fShaderFile;
-	// ensure ifstream objects can throw exceptions:
-	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	try {
-		// open files
-		vShaderFile.open(vertexPath);
-		fShaderFile.open(fragmentPath);
-		std::stringstream vShaderStream, fShaderStream;
-		// read file's buffer contents into streams
-		vShaderStream << vShaderFile.rdbuf();
-		fShaderStream << fShaderFile.rdbuf();
-		// close file handlers
-		vShaderFile.close();
-		fShaderFile.close();
-		// convert stream into string
-		vertexCode = vShaderStream.str();
-		fragmentCode = fShaderStream.str();
-	}
-	catch (std::ifstream::failure e) {
-		std::cout << "File not found!" << std::endl;
-	}
-	const char* vShaderCode = vertexCode.c_str();
-	const char * fShaderCode = fragmentCode.c_str();
-	// 2. compile shaders
-	unsigned int vertex, fragment;
-	// vertex shader
-	vertex = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertex, 1, &vShaderCode, NULL);
-	glCompileShader(vertex);
-	checkCompileErrors(vertex, "vertex");
-	// fragment Shader
-	fragment = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragment, 1, &fShaderCode, NULL);
-	glCompileShader(fragment);
-	checkCompileErrors(fragment, "fragment");
-	// shader Program
-	ID = glCreateProgram();
-	glAttachShader(ID, vertex);
-	glAttachShader(ID, fragment);
-	glLinkProgram(ID);
-	checkCompileErrors(ID, "program");
-	// delete the shaders as they're linked into our program now and no longer necessary
-	glDeleteShader(vertex);
-	glDeleteShader(fragment);
-}
-
-// activate the shader
-// ------------------------------------------------------------------------
-void Shader::use() const {
-	glUseProgram(ID);
-}
-
-// utility uniform functions
-// ------------------------------------------------------------------------
-void Shader::setBool(const std::string &name, bool value) const {
-	glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
-}
-
-// ------------------------------------------------------------------------
-void Shader::setInt(const std::string &name, int value) const {
-	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
-}
-
-// ------------------------------------------------------------------------
-void Shader::setFloat(const std::string &name, float value) const {
-	glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
-}
-
-// ------------------------------------------------------------------------
-void Shader::setVector(const std::string &name, const glm::vec4 &value) const {
-	const GLint loc = glGetUniformLocation(ID, name.c_str());
-	glUniform4fv(loc, 1, glm::value_ptr(value));
-}
-
-// ------------------------------------------------------------------------
-void Shader::setVector(const GLint loc, const glm::vec4 &value) const {
-	glUniform4fv(loc, 1, glm::value_ptr(value));
-}
-
-// ------------------------------------------------------------------------
-void Shader::setMatrix(const std::string &name, const glm::mat3 &value) const {
-	glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, false, glm::value_ptr(value));
-}
-
-void Shader::setMatrix(const std::string &name, const glm::mat4 &value) const {
-	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, false, glm::value_ptr(value));
-}
-
-GLint Shader::getLoc(const std::string &name) const {
-	return glGetUniformLocation(ID, name.c_str());
-}
-
-// utility function for checking shader compilation/linking errors.
-// ------------------------------------------------------------------------
-void Shader::checkCompileErrors(unsigned int shader, std::string type) {
-	int success;
-	char infoLog[1024];
-	if (type != "program") {
-		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-		if (!success) {
-			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
-			std::cout << "\nError compiling " << type << "shader\n" << infoLog << std::endl;
-		}
-	}
-	else {
-		glGetProgramiv(shader, GL_LINK_STATUS, &success);
-		if (!success) {
-			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
-			std::cout << "Error by linking " << type << "\n" << infoLog << std::endl;
-		}
-		else
-		{
-			std::cout << "\t\tOk\n";
-		}
-	}
-}
-
 GLint loadShader(std::string vertexPath, std::string fragmentPath) {
 	std::string vertexCode;
 	std::string fragmentCode;
@@ -170,13 +47,15 @@ GLint loadShader(std::string vertexPath, std::string fragmentPath) {
 		glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
 
 		// The maxLength includes the NULL character
-		std::vector<GLchar> infoLog(maxLength);
-		glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
+		std::string str;
+		str.resize(maxLength);
+		glGetShaderInfoLog(vertexShader, maxLength, &maxLength, str.data());
 
 		// We don't need the shader anymore.
 		glDeleteShader(vertexShader);
 
 		// Use the infoLog as you see fit.
+		std::cout << str;
 
 		// In this simple program, we'll just leave
 		return 0;
@@ -199,8 +78,9 @@ GLint loadShader(std::string vertexPath, std::string fragmentPath) {
 		glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
 
 		// The maxLength includes the NULL character
-		std::vector<GLchar> infoLog(maxLength);
-		glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+		std::string str;
+		str.resize(maxLength);
+		glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, str.data());
 
 		// We don't need the shader anymore.
 		glDeleteShader(fragmentShader);
@@ -208,6 +88,7 @@ GLint loadShader(std::string vertexPath, std::string fragmentPath) {
 		glDeleteShader(vertexShader);
 
 		// Use the infoLog as you see fit.
+		std::cout << str;
 
 		// In this simple program, we'll just leave
 		return 0;
@@ -233,8 +114,9 @@ GLint loadShader(std::string vertexPath, std::string fragmentPath) {
 		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
 
 		// The maxLength includes the NULL character
-		std::vector<GLchar> infoLog(maxLength);
-		glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
+		std::string str;
+		str.resize(maxLength);
+		glGetProgramInfoLog(program, maxLength, &maxLength, str.data());
 
 		// We don't need the program anymore.
 		glDeleteProgram(program);
@@ -243,6 +125,7 @@ GLint loadShader(std::string vertexPath, std::string fragmentPath) {
 		glDeleteShader(fragmentShader);
 
 		// Use the infoLog as you see fit.
+		std::cout << str;
 
 		// In this simple program, we'll just leave
 		return 0;
--- src/shader.h
+++ src/shader.h
@@ -10,29 +10,5 @@
 #include <glm/glm.hpp>
 
 GLint loadShader(std::string vertexPath, std::string fragmentPath);
-class Shader {
-public:
-	// the program ID
-	unsigned int ID;
 
-	// constructor reads and builds the shader
-	Shader() {};
-	Shader(std::string vertexPath, std::string fragmentPath);
-	// use/activate the shader
-	void use() const;
-	// utility uniform functions
-	void setBool(const std::string &name, bool value) const;
-	void setInt(const std::string &name, int value) const;
-	void setFloat(const std::string &name, float value) const;
-	void setVector(const std::string &name, const glm::vec4 &value) const;
-	void setVector(GLint loc, const glm::vec4 &value) const;
-	void setMatrix(const std::string &name, const glm::mat3 &value) const;
-	void setMatrix(const std::string &name, const glm::mat4 &value) const;
-	GLint getLoc(const std::string &name) const;
-private:
-	// utility function for checking shader compilation/linking errors.
-	// ------------------------------------------------------------------------
-	void checkCompileErrors(unsigned int shader, std::string type);
-};
-
-#endif
+#endif SHADER_H
